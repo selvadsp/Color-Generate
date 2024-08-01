6104c24c158fd5045658f543bc6e3f11ab2179e2
selvadsp <selvaprakash28595@gmail.com>
2024-08-01 12:56:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const box = document.getElementById("box");
+const code = document.getElementById("code");
+const btn = document.getElementById("color");
+
+btn.addEventListener('click',colorGenerate);
+
+function colorGenerate(){
+    let values = '0123456789abcdef';
+    let color = '#';
+
+    for(let i=0;i<6;i++){
+        let randomValue = Math.floor(Math.random()*16);
+        color += values[randomValue];
+    }
+
+    code.innerHTML = color;
+    box.style.backgroundColor = color;
+}
